fb1127c2536909c8f0da5f91af674f27cc44efca
The Original G <33421040+OguzDastan@users.noreply.github.com>
2019-05-13 03:55:16 +0200

    Operatør Overview General Structure Established, ezpz

--- ConsumerUWP/Overview.xaml.cs
+++ ConsumerUWP/Overview.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -22,9 +23,94 @@ namespace ConsumerUWP
     /// </summary>
     public sealed partial class Overview : Page
     {
+        private ObservableCollection<NavigationItem> menu = new ObservableCollection<NavigationItem>();
+
         public Overview()
+
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            menu.Clear();
+            menu.Add(new NavigationItem { PageLink = typeof(MainPage), MenuText = typeof(MainPage).Name, MenuIcon = "K2" });
+            menu.Add(new NavigationItem { PageLink = typeof(MainPage), MenuText = typeof(MainPage).Name, MenuIcon = "K3" });
+            menu.Add(new NavigationItem { PageLink = typeof(MainPage), MenuText = typeof(MainPage).Name, MenuIcon = "K4" });
+            menu.Add(new NavigationItem { PageLink = typeof(MainPage), MenuText = typeof(MainPage).Name, MenuIcon = "K5" });
+            menu.Add(new NavigationItem { PageLink = typeof(MainPage), MenuText = typeof(MainPage).Name, MenuIcon = "K6" });
+            menu.Add(new NavigationItem { PageLink = typeof(K7), MenuText = typeof(K7).Name, MenuIcon = "K7" });
+            menu.Add(new NavigationItem { PageLink = typeof(MainPage), MenuText = typeof(MainPage).Name, MenuIcon = "K8" });
+            menu.Add(new NavigationItem { PageLink = typeof(MainPage), MenuText = typeof(MainPage).Name, MenuIcon = "K11" });
+            menu.Add(new NavigationItem { PageLink = typeof(MainPage), MenuText = typeof(MainPage).Name, MenuIcon = "K12" });
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            listmenu.SelectedIndex = 0;
+        }
+
+        private void Button_Click_Pane(object sender, RoutedEventArgs e)
+        {
+            this.RootSpiltView.IsPaneOpen = !this.RootSpiltView.IsPaneOpen;
+        }
+
+        private void Button_Click_Back(object sender, RoutedEventArgs e)
+        {
+            if (splitviewContent.CanGoBack)
+            {
+                splitviewContent.GoBack();
+            }
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var naviitem = listmenu.SelectedItem as NavigationItem;
+            splitviewContent.Navigate(naviitem.PageLink);
+        }
+
+        private void splitviewContent_Navigated(object sender, NavigationEventArgs e)
+        {
+            var page = splitviewContent.CurrentSourcePageType.Name;
+            switch (page)
+            {
+                case "Page1":
+                    listmenu.SelectedIndex = 0;
+                    break;
+
+                case "Page2":
+                    listmenu.SelectedIndex = 1;
+                    break;
+
+                case "Page3":
+                    listmenu.SelectedIndex = 2;
+                    break;
+
+                case "Page4":
+                    listmenu.SelectedIndex = 3;
+                    break;
+
+                case "Page5":
+                    listmenu.SelectedIndex = 3;
+                    break;
+
+                case "Page6":
+                    listmenu.SelectedIndex = 3;
+                    break;
+
+                case "Page7":
+                    listmenu.SelectedIndex = 3;
+                    break;
+
+                case "Page8":
+                    listmenu.SelectedIndex = 3;
+                    break;
+
+                case "Page9":
+                    listmenu.SelectedIndex = 3;
+                    break;
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
